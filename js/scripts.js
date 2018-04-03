32bb8bf1b72f2cd09ded7263a4e98112d2010856
Azamat Bekmuratov and Alex Noble <azamat@>
2018-04-03 15:09:47 -0700

    add functionality for finding the current balance from a username

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,91 @@
+var accounts=[];
+
 
 function bankAccount(accountName, initialDeposit){
   this.accountName=accountName;
-  this.initialDeposit=initialDeposit;
+  this.balance=initialDeposit;
 }
+
+bankAccount.prototype.getBalance = function(inputName) {
+  if (this.accountName===inputName){
+    return this.balance;
+  }
+}
+
+// function searchForAccount(searchName) {
+//   accounts.forEach(function(account){
+//     if (account.accountName === searchName)
+//     {
+//       return account;
+//     }
+//   });
+// }
+
+// bankAccount.prototype.deposit = function() {
+//   if (this.accountName===inputName) {
+//     this.balance += depositAmount;
+//   } else{
+//     alert("Customer account not found. Please register");
+//   }
+// }
+//
+// bankAccount.prototype.withdrawal = function() {
+//   return this.balance
+//   if (this.accountName===inputName) {
+//     this.balance -= withdrawalAmount;
+//   } else{
+//     alert("Customer account not found. Please register");
+//   }
+// }
 
 
 
 $(document).ready(function() {
   $("form#newAccount").submit(function(event) {
-
     var accountName=$("#nameBox").val();
     var initialDeposit = parseFloat($("#initialDepositBox").val());
-
     var newBankAccount=new bankAccount(accountName, initialDeposit);
 
-    console.log(newBankAccount);
     event.preventDefault();
+    accounts.push(newBankAccount);
+
+    console.log(accounts);
 
   });
+
+  $("form#signIn").submit(function(event) {
+    event.preventDefault();
+    var currentAccountName=$("#signInNameBox").val();
+    console.log("LOGIN NAME: "+currentAccountName);
+    var foundAccount = searchForAccount(currentAccountName);
+
+  //  var curBalance = newBankAccount.getBalance(currentAccountName);
+  accounts.forEach(function(account){
+    if (account.accountName === currentAccountName)
+    {
+      console.log(account.balance);
+      console.log("ACCOUNT FOUND");
+    }
+    // else{
+    //   console.log("name not found");
+    // }
+  });
+
+    $("#signIn").toggle();
+    $("#newAccount").toggle();
+    $("#deposit").toggle();
+    $("#withdrawal").toggle();
+    $("#currentBalance").toggle();
+
+
+    //console.log(curBalance);
+
+    //$("#currentBalance").getBalance(currentAccountName);
+    //$("#currentBalance").append(accounts.find(""));
+
+  });
+
+
+
+
 });
